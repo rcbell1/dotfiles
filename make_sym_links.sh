90fefc00ef4ae4cd62d681151e829e3a8d9e6cb5
Richard Bell <rcbell@eng.ucsd.edu>
2022-12-26 14:46:17 -0800

    setbuffs is used to set the network buffers to what SDRs need for highspeed operation. make_sym_links.sh automates the process of making all the symlinks on a fresh install

---
+++ make_sym_links.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ln -s .bash_aliases ../.bash_aliases
+ln -s .git ../.git
+ln -s .gitconfig ../.gitconfig
+ln -s .tmux ../.tmux
+ln -s .tmux.conf ../.tmux.conf
+ln -s .vim ../.vim
+ln -s .vimrc ../.vimrc
