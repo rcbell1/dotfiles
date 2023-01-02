85dfdf1b4108f665acc6f7d9d9d69a95a85f5ea0
Richard Bell <rcbell@eng.ucsd.edu>
2023-01-02 04:53:23 -0800

    starship makes a pretty bash prompt that is easy to install and customize

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+curl -sS https://starship.rs/install.sh | sh
+sudo apt install fonts-powerline
 
 ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-#ln -s .tmux ../.tmux
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-#ln -s .vim ../.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+mkdir -p ~/.config && ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
