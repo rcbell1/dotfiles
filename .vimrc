d21624b1bdef9b051a767671d5782eb09ac62297
Richard Bell <rcbell@eng.ucsd.edu>
2023-01-03 00:00:43 -0800

    added needed setup to install plugin managers for vim and tmux and download a nerd font for starship

--- .vimrc
+++ .vimrc
@@ -1,3 +1,10 @@
+"This code automates the process of making sure vimplug is available to vim
+let data_dir = has('nvim') ? stdpath('data') . '/site' : '~/.vim'
+if empty(glob(data_dir . '/autoload/plug.vim'))
+  silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+endif
+
 "To update/install plugins type :PlugInstall inside vim
 call plug#begin('~/.vim/plugged')
 Plug 'vim-airline/vim-airline'
--- setup.sh
+++ setup.sh
@@ -2,7 +2,14 @@
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
 curl -sS https://starship.rs/install.sh | sh
-sudo apt install fonts-powerline
+
+# download and install nerd font, needed for glyphs used by starship
+curl https://github.com/ryanoasis/nerd-fonts/blob/5c5c51e7b18eb080f1fa24df9d164a4b6ff62a6c/patched-fonts/UbuntuMono/Regular/complete/Ubuntu%20Mono%20Nerd%20Font%20Complete%20Mono%20Windows%20Compatible.ttf -O
+sudo mv Ubuntu%20Mono%20Nerd%20Font%20Complete%20Mono%20Windows%20Compatible.ttf /usr/share/fonts
+fc-cache -f -v
+
+# clone the tmux plugin manager tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
