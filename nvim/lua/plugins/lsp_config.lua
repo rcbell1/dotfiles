a0b7f6fdb9c1aebde10e9aeeb1e48382d561bb8a
Richard Bell <rcbell@eng.ucsd.edu>
2023-03-05 02:13:56 -0800

    Added a border to hover boxes

--- nvim/lua/plugins/lsp_config.lua
+++ nvim/lua/plugins/lsp_config.lua
@@ -47,7 +47,23 @@ mason_lspconfig.setup_handlers {
   end,
 }
 
+local _border = "single"
 
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    border = _border
+  }
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help, {
+    border = _border
+  }
+)
+
+vim.diagnostic.config{
+  float={border=_border}
+}
 -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- require("lspconfig").lua_ls.setup {
