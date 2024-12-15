4e5113a6742a319df964c5c24a87e4030ea3cc2a
Richard Bell <rcbell@ucsd.edu>
2024-12-15 12:43:51 -0800

    Updated absolute copy paths to programatic

--- setup.sh
+++ setup.sh
@@ -15,9 +15,9 @@ curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-pro
 # download and install nerd font, needed for glyphs used by starship
 FILE=/usr/share/fonts/UbuntuMonoNerdFontMono-Regular.ttf
 if [ ! -f "$FILE" ]; then
-	# sudo curl -L https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/UbuntuMono/Regular/complete/Ubuntu%20Mono%20Nerd%20Font%20Complete%20Mono.ttf  -o "$FILE"
-	sudo curl -L https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/UbuntuMono/Regular/UbuntuMonoNerdFontMono-Regular.ttf -o "$FILE"
-	fc-cache -f -v
+  # sudo curl -L https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/UbuntuMono/Regular/complete/Ubuntu%20Mono%20Nerd%20Font%20Complete%20Mono.ttf  -o "$FILE"
+  sudo curl -L https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/UbuntuMono/Regular/UbuntuMonoNerdFontMono-Regular.ttf -o "$FILE"
+  fc-cache -f -v
 fi
 
 # clone the tmux plugin manager tpm, Ctrl+b Shift+i to install plugins
@@ -40,7 +40,7 @@ rm ripgrep_13.0.0_amd64.deb
 
 # nodejs and npm support for LSP servers and Copilot in neovim
 # Be sure to type ':Copilot setup' in neovim to install the Copilot binary
-curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
+curl -sL https://deb.nodesource.com/setup_20.x | sudo bash -
 cat /etc/apt/sources.list.d/nodesource.list
 sudo apt -y install nodejs
 sudo apt -y install build-essential
@@ -66,16 +66,16 @@ sudo apt-get -y install -f
 rm google-chrome*.deb
 
 # create symlinks to dotfiles if they don't already exist
-[ -f ~/.bash_profile ] || ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-[ -f ~/.bash_aliases ] || ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
-[ -f ~/.clang-format ] || ln -s ~/dotfiles/.clang-format ~/.clang-format
-[ -f ~/.gitconfig ] || ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-[ -f ~/.tmux.conf ] || ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-[ -f ~/.vimrc ] || ln -s ~/dotfiles/.vimrc ~/.vimrc
-mkdir -p ~/.config && [ -f ~/.config/starship.toml ] || ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
-[ -d ~/.config/nvim ] || ln -s ~/dotfiles/nvim ~/.config/nvim
+[ -f ~/.bash_profile ] || ln -s "$(realpath "$(dirname "${BASH_SOURCE[0]}")")/.bash_profile" ~/.bash_profile
+[ -f ~/.clang-format ] || ln -s "$(realpath "$(dirname "${BASH_SOURCE[0]}")")/.clang-format" ~/.clang-format
+[ -f ~/.bash_aliases ] || ln -s "$(realpath "$(dirname "${BASH_SOURCE[0]}")")/.bash_aliases" ~/.bash_aliases
+[ -f ~/.gitconfig ] || ln -s "$(realpath "$(dirname "${BASH_SOURCE[0]}")")/.gitconfig" ~/.gitconfig
+[ -f ~/.tmux.conf ] || ln -s "$(realpath "$(dirname "${BASH_SOURCE[0]}")")/.tmux.conf" ~/.tmux.conf
+[ -f ~/.vimrc ] || ln -s "$(realpath "$(dirname "${BASH_SOURCE[0]}")")/.vimrc" ~/.vimrc
+mkdir -p ~/.config && [ -f ~/.config/starship.toml ] || ln -s "$(realpath "$(dirname "${BASH_SOURCE[0]}")")/starship.toml" ~/.config/starship.toml
+[ -d ~/.config/nvim ] || ln -s "$(realpath "$(dirname "${BASH_SOURCE[0]}")")/nvim" ~/.config/nvim
 [ -f /usr/bin/bat ] || sudo ln -s /usr/bin/batcat /usr/bin/bat
-[ -f ~/.ssh/config ] || sudo ln -s ~/dotfiles/config ~/.ssh/config
+[ -f ~/.ssh/config ] || sudo ln -s "$(realpath "$(dirname "${BASH_SOURCE[0]}")")/config" ~/.ssh/config
 [ -f ~/.bashrc ] || cp /etc/skel/.bashrc ~/.bashrc
 
 source ~/.bashrc
